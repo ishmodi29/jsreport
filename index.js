9212d20865f4c967ba98b152ef0293f7093d4ab8
ish <59726394+ishmodi29@users.noreply.github.com>
2023-06-03 14:04:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const logsDir = path.join(__dirname, 'logs');
+
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
 const jsreport = require('jsreport')({
   httpPort: process.env.PORT || 3000,
   extensions: {
